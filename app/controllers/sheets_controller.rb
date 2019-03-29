32cf3ca6e27d61ef947d4f3ad51a208a728c795f
bannmoore <moore.brittanyann@gmail.com>
2019-03-29 12:07:39 -0400

    feat: Add new, edit, and list views and pathing for character sheets.

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,10 +1,36 @@
 class SheetsController < ApplicationController
   http_basic_authenticate_with name: 'bannmoore', password: 'secret', only: :destroy
 
+  def show
+    @character = Character.find(params[:character_id])
+    @sheet = @character.sheets.find(params[:id])
+  end
+
+  def new
+    @character = Character.find(params[:character_id])
+    @sheet = @character.sheets.new
+  end
+
+  def edit
+    @character = Character.find(params[:character_id])
+    @sheet = @character.sheets.find(params[:id])
+  end
+
   def create
     @character = Character.find(params[:character_id])
     @sheet = @character.sheets.create(sheet_params)
     redirect_to character_path(@character)
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    @sheet = @character.sheets.find(params[:id])
+
+    if @sheet.update(sheet_params)
+      redirect_to character_sheet_path(@sheet)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
